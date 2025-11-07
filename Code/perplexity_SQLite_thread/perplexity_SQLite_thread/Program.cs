bbf4a6f20ffbc9be0f969b7a5819f382d123938a
Jash.Liao <jash.liao@gmail.com>
2025-11-07 10:16:32 +0800

    Modified 202511071016

--- Code/perplexity_SQLite_thread/perplexity_SQLite_thread/Program.cs
+++ Code/perplexity_SQLite_thread/perplexity_SQLite_thread/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SQLite;
+using System.Diagnostics;
 using System.Threading;
 
 //---
@@ -11,26 +12,66 @@ class Program
     private static readonly object dbLock = new object();
 
     private static string connectionString = "Data Source=mydatabase.db;Version=3;Pooling=True;";
+    private static Stopwatch m_stopWatch = new Stopwatch();
+    private static String m_StrTitle = "";
+    private static String m_StrStartFileLine = "";
+    private static String m_StrEndFileLine = "";
+    public static void Start(String StrInfor)
+    {
+        StackFrame CallStack = new StackFrame(1, true);
+        m_StrStartFileLine = String.Format("File : {0} , Line : {1}", CallStack.GetFileName(), CallStack.GetFileLineNumber());
+        m_StrTitle = StrInfor;
 
+        m_stopWatch.Start();
+    }
+    public static string Stop()
+    {
+        StackFrame CallStack = new StackFrame(1, true);
+
+        m_stopWatch.Stop();
+
+        // Get the elapsed time as a TimeSpan value.
+        TimeSpan ts = m_stopWatch.Elapsed;
+        // Format and display the TimeSpan value.
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+        m_StrEndFileLine = String.Format("File : {0} , Line : {1}", CallStack.GetFileName(), CallStack.GetFileLineNumber());
+
+        return m_StrStartFileLine + " ~ " + m_StrEndFileLine + " consume time: " + elapsedTime + "~ FileName:" + m_StrTitle;
+    }
+    static void Pause()
+    {
+        Console.Write("Press any key to continue...");
+        Console.ReadKey(true);
+    }
     static void Main()
     {
         // 建立資料表（確保資料庫已存在並有表）
         InitializeDatabase();
 
+        Start("使用 20 個執行緒同時寫入500筆資料 = 共10000筆");
+        Console.WriteLine("開始平行寫入...");
+        
         // 建立多個執行緒模擬同時寫入
-        Thread t1 = new Thread(() => InsertData("Thread 1"));
-        Thread t2 = new Thread(() => InsertData("Thread 2"));
-        Thread t3 = new Thread(() => InsertData("Thread 3"));
+        Thread[] Tall=new Thread[20];
+        for (int i = 0; i < Tall.Length; i++)
+        {
+            Tall[i] = new Thread(() => InsertData($"Thread {i}"));
+        }
 
-        t1.Start();
-        t2.Start();
-        t3.Start();
+        for (int i = 0; i < Tall.Length; i++)
+        {
+            Tall[i].Start();
+        }
 
-        t1.Join();
-        t2.Join();
-        t3.Join();
+        for (int i = 0; i < Tall.Length; i++)
+        {
+            Tall[i].Join();
+        }
 
         Console.WriteLine("Finished.");
+        Console.WriteLine(Stop());
+        Pause();
     }
 
     static void InitializeDatabase()
@@ -48,7 +89,7 @@ class Program
 
     static void InsertData(string message)
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 500; i++)
         {
             lock (dbLock)
             {
@@ -69,7 +110,7 @@ class Program
                     }
                 }
             }
-            Thread.Sleep(100); // 模擬作業間隔
+            //Thread.Sleep(100); // 模擬作業間隔
         }
     }
 }
